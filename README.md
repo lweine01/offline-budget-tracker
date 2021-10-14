bf11ebfa71542f6293e8e5379eb12c59a9fb83e7
lweine01 <laurarweiner@gmail.com>
2021-10-14 17:48:46 -0400

    updated README, fixed formatting in indexdb.js

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# offline-budget-tracker
+# Online Offline Budget Tracker
+
+[Link to Deployed Site]()
+
+## Table of Contents
+- [Description](#Description)
+- [Usage](#Usage)
+- [Screenshots](#Screenshots)
+- [License](#License)
+- [Languages](#Languages)
+- [Questions](#Questions)
+
+## Description
+This application is a budget tracker where the user will be able to add expenses and deposits to their budget with or without a connection. When entering transactions offline, they should populate the total when brought back online.
+
+## Usage
+To allow the user to use their budget application even when offline. It will store data so it can still be accessed when brought back online. This is helpful for people who often travel.
+
+## Screenshot
+![screenshot](assets/images/screenshot1.png)
+![screenshot](assets/images/screenshot2.png)
+
+-------
+
+## License
+MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Languages
+JavaScript, HTML, CSS, MongoDB
+
+## Questions
+Please contact me at lauraRweiner@gmail.com if you have any questions or suggestions.
--- public/indexdb.js
+++ public/indexdb.js
@@ -8,8 +8,6 @@ request.onupgradeneeded = event => {
 
 request.onsuccess = event => {
   db = event.target.result;
-
-  // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
@@ -20,22 +18,14 @@ request.onerror = event => {
 };
 
 const saveRecord = record => {
-  // create a transaction on the pending db with readwrite access
   const transaction = db.transaction(["pending"], "readwrite");
-
-  // access your pending object store
   const store = transaction.objectStore("pending");
-
-  // add record to your store with add method.
   store.add(record);
 }
 
 const checkDatabase = () => {
-  // open a transaction on your pending db
   const transaction = db.transaction(["pending"], "readwrite");
-  // access your pending object store
   const store = transaction.objectStore("pending");
-  // get all records from store and set to a variable
   const getAll = store.getAll();
 
   getAll.onsuccess = () => {
@@ -50,18 +40,13 @@ const checkDatabase = () => {
       })
       .then(response => response.json())
       .then(() => {
-        // if successful, open a transaction on your pending db
         const transaction = db.transaction(["pending"], "readwrite");
-
-        // access your pending object store
         const store = transaction.objectStore("pending");
 
-        // clear all items in your store
         store.clear();
       });
     }
   };
 }
 
-// listen for app coming back online
 window.addEventListener("online", checkDatabase);
